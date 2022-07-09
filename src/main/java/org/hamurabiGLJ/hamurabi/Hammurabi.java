3ebde01cb8098299ba74eee4403acfafba085dc7
Git-Yee <git-yee@ZipcodersMBP58.fios-router.home>
2022-07-09 17:49:29 -0400

    remove extra lines

--- src/main/java/org/hamurabiGLJ/hamurabi/Hammurabi.java
+++ src/main/java/org/hamurabiGLJ/hamurabi/Hammurabi.java
@@ -304,20 +304,15 @@ public class Hammurabi {
     }
 
     int grainEatenByRats(int bushels) {
-
-        //here is a 40% chance that you will have a rat`` infestation.
-        // When this happens, rats will eat somewhere between 10% and 30% of your grain.
-        // Return the amount of grain eaten by rats (possibly zero).
         int chanceRatInfestation =  rand.nextInt(0,101);
-        System.out.println(chanceRatInfestation);
+
         if (chanceRatInfestation <= 40) {
             int eatenByRat = rand.nextInt(10,31);
-            System.out.println(eatenByRat);
-            System.out.println("O Great Hammurabi, there is an infestation! The rats have eaten " +
-                    (bushels*eatenByRat/100) + " bushels!");
+//            System.out.println("O Great Hammurabi, there is an infestation! The rats have eaten " +
+//                    (bushels*eatenByRat/100) + " bushels!");
             return (bushels*eatenByRat/100);
         }
-        System.out.println("O Great Hammurabi, the rats have not eaten any of our bushels!");
+//        System.out.println("O Great Hammurabi, the rats have not eaten any of our bushels!");
         return 0;
     }
 
