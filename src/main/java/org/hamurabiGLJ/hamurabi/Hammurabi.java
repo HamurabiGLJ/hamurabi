5178f30cc54ab2ee2b6a49b32680d7db915401ba
Git-Yee <git-yee@ZipcodersMBP58.fios-router.home>
2022-07-09 12:22:21 -0400

    working on methods

--- src/main/java/org/hamurabiGLJ/hamurabi/Hammurabi.java
+++ src/main/java/org/hamurabiGLJ/hamurabi/Hammurabi.java
@@ -13,12 +13,24 @@ public class Hammurabi {
 
     void playGame() {
         // declare local variables here: grain, population, etc.
-        Integer grain = 2800;
-        Integer population = 100;
-        Integer land = 1000;
+        Integer grain = 100;
+        Integer population = 4;
+        Integer land = 400;
         Integer landValue = 19;
 
+//        Each person needs at least 20 bushels of grain per year to survive
+//        Each person can farm at most 10 acres of land
+//        It takes 2 bushels of grain to farm an acre of land
+//        The market price for land fluctuates yearly
+
+//        100 people
+//        2800 bushels of grain in storage
+//        1000 acres of land
+//        Land value is 19 bushels/acre
+
         // statements go after the declarations
+
+        askHowManyAcresToPlant(land, population, grain);
     }
 
     int getNumber(String message) {
@@ -32,13 +44,73 @@ public class Hammurabi {
             }
         }
     }
-    int askHowManyAcresToBuy(int price, int bushels) {return 0;}
-    //other methods go here
+    int askHowManyAcresToBuy(int price, int bushels) {
+        int maxAcres = bushels / price;
+        String msg = "How many acres of land would you like to buy?";
+        int input = getNumber(msg);
+        while (input > maxAcres) {
+            String newMsg = String.format("O Great Hammurabi, surely you jest! We'd need %d bushels but we only have %d!",
+                    input * price, bushels);
+            System.out.println(newMsg);
+            input = getNumber(msg);
+        }
+        return input;
+    }
+
     int askHowManyAcresToSell(int acresOwned) {return 0;}
 
-    int askHowMuchGrainToFeedPeople(int bushels) {return 0;}
+    int askHowMuchGrainToFeedPeople(int bushels) {
+        //Ask the player how much grain to feed people, and returns that number.
+        // You can't feed them more grain than you have. You can feed them more than they need to survive.
+        String msg = "How many bushels of grain do you want to feed your people?\n";
+        int input = getNumber(msg);
+        while (input > bushels) {
+            String newMsg = String.format("O Great Hammurabi, surely you jest! We'd need %d bushels but we only have %d!",
+                    input, bushels);
+            System.out.println(newMsg);
+            input = getNumber(msg);
+        }
+        return input;
+    }
 
-    int askHowManyAcresToPlant(int acresOwned, int population, int bushels) {return 0;};
+    int askHowManyAcresToPlant(int acresOwned, int population, int bushels) {
+//        Ask the player how many acres to plant with grain, and returns that number.
+//        You must have enough acres, enough grain, and enough people to do the planting.
+//        Any grain left over goes into storage for next year.
+
+        //        Integer grain = 2800;
+        //        Integer population = 100;
+        //        Integer land = 1000;
+        //        Integer landValue = 19;
+
+        String msg = "How many acres do you want to plant with grain?\n";
+        int input = getNumber(msg);
+
+        while (true) {
+            if (input > acresOwned) {
+                String newMsg = String.format("O Great Hammurabi, surely you jest! We'd need %d acres but we only have %d!",
+                        input, acresOwned);
+                System.out.println(newMsg);
+                input = getNumber(msg);
+                continue;
+            }
+            if (input/10 > population) {
+                String newMsg = String.format("O Great Hammurabi, surely you jest! We'd need %d people but we only have %d!",
+                        input/10 , population);
+                System.out.println(newMsg);
+                input = getNumber(msg);
+                continue;
+            }
+            if (input > bushels/2) {
+                String newMsg = String.format("O Great Hammurabi, surely you jest! We'd need %d bushels but we only have %d!",
+                        input*2, bushels);
+                System.out.println(newMsg);
+                input = getNumber(msg);
+                continue;
+            }
+            return input;
+        }
+    }
 
     int plagueDeaths(int population) {return 0;}
 
